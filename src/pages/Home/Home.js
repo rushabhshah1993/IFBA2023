714cd395b4e421782c153fea26509e10f8f36b8c
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2023-11-21 02:09:32 +0530

    Adding guests infomation (Part 1)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,7 +16,9 @@ const Home = () => {
     const [currentMsg, setCurrentMsg] = useState(messages[0]);
     const [checkedInGuests, setCheckedInGuests] = useState([]);
     const [checkedInPlusOnesCount, setCheckedInPlusOnesCount] = useState(0);
+    const [pendingGuests, setPendingGuests] = useState([]);
     const guests = useSelector(state => state.guests.guests);
+    let checkedInElement, pendingGuestsElement;
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -37,12 +39,58 @@ const Home = () => {
     }, []);
 
     useEffect(() => {
-        console.log("guests:   ", guests);
         let checkedInGuests = guests.filter(item => item.entry);
+        let pendingGuests = guests.filter(item => !item.entry);
         let plusOnes = checkedInGuests.reduce((total, item) => total + item.plusOnes, 0);
         setCheckedInPlusOnesCount(plusOnes);
         setCheckedInGuests(checkedInGuests);
-    }, [guests])
+        setPendingGuests(pendingGuests);
+    }, [guests]);
+
+    if(checkedInGuests.length) {
+        checkedInElement = (
+            <div className={styles.checkedInWrapper}>
+                {
+                    checkedInGuests.map((guest, index) => (
+                        <div key={guest.id} className={styles.checkedInGuest}>
+                            <div>{index+1}</div>
+                            <div>{guest.firstName} {guest.lastName}</div>
+                            <div>{guest.plusOnesEntered}</div>
+                            <div>{guest.checkIn.checkedInBy}</div>
+                            <div>{new Date(guest.checkIn.checkedInAt).toTimeString()}</div>
+                        </div>
+                    ))
+                }
+            </div>
+        )
+    } else {
+        checkedInElement = (
+            <div className={styles.nothingToShowHere}>
+                No guests have been checked-in yet.
+            </div>
+        )
+    }
+
+    if(pendingGuests.length) {
+        pendingGuestsElement = (
+            <div className={styles.pendingGuests}>
+                {
+                    pendingGuests.map((guest, index) => (
+                        <div key={guest.id} className={styles.pendingGuest}>
+                            <div>{index+1}</div>
+                            <div>{guest.firstName} {guest.lastName} with {guest.plusOnes} guests</div>
+                        </div>
+                    ))
+                }
+            </div>
+        )
+    } else {
+        pendingGuestsElement = (
+            <div className={styles.nothingToShowHere}>
+                All the guests have arrived! 🎉
+            </div>
+        )
+    }
 
     return (
         <div className={styles.homeContainer}>
@@ -63,6 +111,19 @@ const Home = () => {
                     label={'Pending Guests'} 
                     value={+guests.length - +checkedInGuests.length} 
                     className={styles.metric} />
+            </div>
+
+            <div className={styles.guestsCounter}>
+                <div className={styles.guestsContainer}>
+                    <div className={styles.checkedInContainer}>
+                        <p className={styles.guestsHeader}>Guests Log</p>
+                        { checkedInElement }
+                    </div>
+                    <div className={styles.yetToArriveCounter}>
+                        <p className={styles.guestsHeader}>Yet To Arrive ({pendingGuests.length} invites)</p>
+                        { pendingGuestsElement }
+                    </div>
+                </div>
             </div>
         </div>
     )
